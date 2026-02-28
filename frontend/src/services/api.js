fbd4a72af4dae69793bf8f8d72d850ebf373bdf8
Bambo0o0o <pramote.s@windowslive.com>
2026-02-28 19:59:29 +0700

    Solving : npm error code ERESOLVE

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,18 +1,7 @@
 import axios from "axios";
-import Dashboard from "./pages/Dashboard";
 
 const API = axios.create({
-  // Offline URL
-  // baseURL: "http://localhost:5000/api/v1",
-  // Online URL
-   baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "http://localhost:5000/api/v1",
 });
-
-function App() {
-  return <Dashboard />;
-}
-
-// Temporay Adding when setup REACT_APP_API_URL
-console.log("API URL:", process.env.REACT_APP_API_URL);
 
 export default API;
