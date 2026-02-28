009ef4c06aa522cffdf656b2f00a7795c91bb8de
Bambo0o0o <pramote.s@windowslive.com>
2026-02-28 19:14:47 +0700

    Complete Render : Config Vercel for frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,17 @@ const chartRoutes = require("./routes/chartRoutes");
 
 const app = express();
 
-app.use(cors());
+// Original Setup
+// app.use(cors());
+
+// To checking cores backend
+const cors = require("cors");
+
+app.use(cors({
+  origin: "https://your-vercel-project.vercel.app"
+}));
+
+
 app.use(express.json());
 
 app.use("/api/v1/charts", chartRoutes);
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,11 +2,17 @@ import axios from "axios";
 import Dashboard from "./pages/Dashboard";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  // Offline URL
+  // baseURL: "http://localhost:5000/api/v1",
+  // Online URL
+   baseURL: process.env.REACT_APP_API_URL,
 });
 
 function App() {
   return <Dashboard />;
 }
+
+// Temporay Adding when setup REACT_APP_API_URL
+console.log("API URL:", process.env.REACT_APP_API_URL);
 
 export default API;
